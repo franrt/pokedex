71f72940d31d7912be0c181bf4c989498a8b702d
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-08-12 20:40:38 -0400

    aparecen todos los sprites de pokemon ordenados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
-$.ajax({
-        url: 'http://pokeapi.co/api/v2/pokedex/1/',
-        type: 'GET',
-        dataType: 'JSON',
-        data: { 'limit': '811' },
-    })
-    .done(function(result) {
+$(document).ready(function() {
 
-        result.pokemon_entries.forEach(function(el) {
-            console.log(el.pokemon_species.name)
-        });
-    })
-    .fail(function() {
-        console.log('error')
+    $.get('https://pokeapi.co/api/v2/pokedex/1/', function(result) {
+        console.log(result);
+        result.pokemon_entries.forEach(function(pokemon) {
+            var name = JSON.stringify(pokemon.pokemon_species.name); //al hacer stringify devuelve el nombre de 721 Pokes
+
+            var obj = JSON.parse(name.replace(/(\{|,)\s*(.+?)\s*:/g, '$1 "$2":')); //para poder pasar el nombre sin comillas 
+            //console.log(obj);
+            //conseguir sprites de los pokemon
+
+            var img = 'https://img.pokemondb.net/sprites/x-y/normal/' + obj + '.png ';
+
+            $('.poke').append(`<div class="col s4 m4 l4 imagenes"><img src="${img}" alt=${name}></div>`);
+
+
+
+        })
     });
+
+});
